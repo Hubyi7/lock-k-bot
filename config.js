a7493bdaeba2d5b88abcc29a31655523f48ef2ae
HUBY <106633028+Hubyi7@users.noreply.github.com>
2023-01-10 15:49:43 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    //////////////////////////
+
+    token: "", //WRITE YOUR BOT TOKEN
+
+    botStatus: "Lock K executor",
+
+  //  user: ""
+
+        //WRITE YOUR BOT STATUS.
+
+    //////////////////////////
+
+}
